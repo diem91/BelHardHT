d5bdd2350298c3f4139ebd54c82c122568afd71a
diem91 <145827324+diem91@users.noreply.github.com>
2024-03-11 15:35:09 +0300

    main.py

---
+++ lesson_3/main.py
@@ -0,0 +1,25 @@
+# 1
+firstname = input("Введите имя: ")
+lastname = input("Введите фамилию: ")
+age = input("Введите возраст: ")
+print(f"Привет, меня зовут {firstname} {lastname}, мне {age} лет")
+
+# 2
+s = input()
+print(s[2])
+
+# 3
+s = input()
+print(s[-2])
+
+# 4
+s = input()
+print(s[0:5])
+
+# 5
+s = input()
+print(s[:-2])
+
+# 6
+some_str = "Hello, world!"
+print(some_str.replace('world', 'Dmitry'))
